846f0fa9e9b1127acd1e6369ca6bffbc739fafa0
Purukitto <purukitto@gmail.com>
2023-09-03 19:14:48 +0530

    Restructure (remove backend)

---
+++ src/components/Chat.jsx
@@ -0,0 +1,10 @@
+import { PlusIcon } from "@heroicons/react/24/solid";
+
+export default function Chat() {
+	return (
+		<div>
+			<PlusIcon className="h-4 w-4" />
+            <p>New chat</p>
+		</div>
+	);
+}
