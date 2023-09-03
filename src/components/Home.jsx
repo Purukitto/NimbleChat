b747fe6b3fa6ab1d90115872632ddc35020bd704
Purukitto <purukitto@gmail.com>
2023-09-03 22:04:10 +0530

    :lipstick: Minor UI improvements

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { SunIcon } from "@heroicons/react/24/outline";
+import { ChatBubbleLeftEllipsisIcon } from "@heroicons/react/24/outline";
 import supabase from "../helper/supabase";
 
 export default function Home() {
@@ -13,10 +13,10 @@ export default function Home() {
 
 	return (
 		<div className="flex flex-col items-center justify-center h-screen px-2 text-white">
-			<h1 className="text-5xl font-bold mb-20">NimbleChat</h1>
+			<h1 className="text-4xl font-bold mb-20">NimbleChat</h1>
 			<div className="text-center">
 				<div className="flex flex-col items-center justify-center mb-5">
-					<SunIcon className="h-8 w-8" />
+					<ChatBubbleLeftEllipsisIcon className="h-8 w-8" />
 					<h2>Examples</h2>
 				</div>
 				<div className="space-y-2">
@@ -28,7 +28,7 @@ export default function Home() {
 					<p className="infoText">Lorem Ipsum yada yada</p>
 				</div>
 			</div>
-			<div>
+			<div className=" mt-24">
 				<button
 					onClick={login}
 					className="py-2 px-4 flex justify-center items-center bg-gray-600 hover:bg-gray-700 w-full font-semibold shadow-md rounded-lg"
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,13 +1,9 @@
 import { Outlet } from "react-router-dom";
-import Sidebar from "../components/Sidebar";
 
 export default function Main() {
 	return (
 		<div className="flex">
-			<Sidebar />
-
-			{/* TODO: ClientProvider */}
-			<div className="bg-[#343541] flex-1">
+			<div className="bg-[#343541] flex-1 h-screen">
 				<Outlet />
 			</div>
 		</div>
