45906c17896a598f100e94ed7ad00996b6c03eb6
Purukitto <purukitto@gmail.com>
2023-09-04 06:52:14 +0530

    :art: Use session info instead of props

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+import supabase from "../helper/supabase";
+
 export default function Chat() {
+	const [user, setUser] = useState(null); // User details
+
+	useEffect(() => {
+		supabase.auth.getSession().then((res) => {
+			const session = res.data.session;
+			setUser(session.user);
+		});
+	}, []);
+
 	return (
 		<div className="flex-1">
 			<h1>Hi</h1>
