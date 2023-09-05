903cf285f9282f751635e454e2b2ce6414355a5c
Purukitto <purukitto@gmail.com>
2023-09-05 08:01:57 +0530

    Try cors anywhere

--- src/helper/nbxCall.js
+++ src/helper/nbxCall.js
@@ -1,27 +1,30 @@
 export default function nbxCall(input) {
-	const response = fetch("https://chat.nbox.ai/api/chat/completions", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-			Authorization: import.meta.env.VITE_NBX_KEY,
-		},
-		body: JSON.stringify({
-			temperature: 0.5,
-			messages: [
-				{
-					role: "system",
-					content: "You are a weather bot. Respond accordingly.",
-				},
-				{
-					role: "user",
-					content: input,
-				},
-			],
-			model: "llama-2-chat-13b-4k",
-			stream: true,
-			max_tokens: 1000,
-		}),
-	});
+	const response = fetch(
+		"https://cors-anywhere.herokuapp.com/https://chat.nbox.ai/api/chat/completions",
+		{
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: import.meta.env.VITE_NBX_KEY,
+			},
+			body: JSON.stringify({
+				temperature: 0.5,
+				messages: [
+					{
+						role: "system",
+						content: "You are a weather bot. Respond accordingly.",
+					},
+					{
+						role: "user",
+						content: input,
+					},
+				],
+				model: "llama-2-chat-13b-4k",
+				stream: true,
+				max_tokens: 1000,
+			}),
+		}
+	);
 
 	console.log(response);
 }
