103c21cff90fe9a596a0bf1efc8ec62347032e1e
Purukitto <purukitto@gmail.com>
2023-09-07 20:02:03 +0530

    :sparkles: Add login state

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -25,24 +25,30 @@ export default function Chat() {
 
 	return (
 		<div className="flex-1 overflow-y-auto overflow-x-hidden">
-			{chat.messages.length > 0 ? (
-				!chat.error ? (
-					<>
-						{chat.messages.map((message) => (
-							<Message key={message.id} message={message} />
-						))}
-						<div ref={ref} className="h-3" />
-					</>
+			{!chat.error ? (
+				!chat.loading ? (
+					chat.messages.length > 0 ? (
+						<>
+							{chat.messages.map((message) => (
+								<Message key={message.id} message={message} />
+							))}
+							<div ref={ref} className="h-3" />
+						</>
+					) : (
+						<>
+							<p className="mt-10 text-xl text-center text-white">
+								Enter promt below to get started!
+							</p>
+							<ArrowDownCircleIcon className="h-10 w-10 mx-auto mt-5 animate-bounce text-white" />
+						</>
+					)
 				) : (
-					<>
-						<p className="mt-10 text-center text-white">
-							Enter promt below to get started!
-						</p>
-						<ArrowDownCircleIcon className="h-10 w-10 mx-auto mt-5 animate-bounce text-white" />
-					</>
+					<p className="mt-10 text-xl text-center text-white">
+						Loading messages...
+					</p>
 				)
 			) : (
-				<p className="mt-10 text-center text-white">
+				<p className="mt-10 text-xl text-center text-white">
 					Some error occured. Please refresh or try again later.
 				</p>
 			)}
