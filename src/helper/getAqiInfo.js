4ebee8f8f3e95ba57bb53fe822d093e1af2ede7f
Purukitto <purukitto@gmail.com>
2023-09-07 15:58:47 +0530

    Function to tell AQI hazard and colour

---
+++ src/helper/getAqiInfo.js
@@ -0,0 +1,20 @@
+export default function getAQIInfo(aqiValue) {
+	if (aqiValue >= 0 && aqiValue <= 50) {
+		return { healthConcern: "Good", color: "#00C564" };
+	} else if (aqiValue <= 100) {
+		return { healthConcern: "Satisfactory", color: "#8DDF5F" };
+	} else if (aqiValue <= 200) {
+		return {
+			healthConcern: "Moderate",
+			color: "#FAFF00",
+		};
+	} else if (aqiValue <= 300) {
+		return { healthConcern: "Poor", color: "#FFA600" };
+	} else if (aqiValue <= 400) {
+		return { healthConcern: "Very Poor", color: "#FF0000" };
+	} else if (aqiValue <= 500) {
+		return { healthConcern: "Severe", color: "#DE0000" };
+	} else {
+		return { healthConcern: "Invalid AQI", color: "Unknown" };
+	}
+}
