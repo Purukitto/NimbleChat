34bad80f2f9a4e4ec6bf3b4e6f04d86be13c9d35
Purukitto <purukitto@gmail.com>
2023-09-07 19:53:02 +0530

    :sparkles: Add Error state component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -25,20 +25,26 @@ export default function Chat() {
 
 	return (
 		<div className="flex-1 overflow-y-auto overflow-x-hidden">
-			{!chat.error && chat.messages.length > 0 ? (
-				<>
-					{chat.messages.map((message) => (
-						<Message key={message.id} message={message} />
-					))}
-					<div ref={ref} className="h-3" />
-				</>
+			{chat.messages.length > 0 ? (
+				!chat.error ? (
+					<>
+						{chat.messages.map((message) => (
+							<Message key={message.id} message={message} />
+						))}
+						<div ref={ref} className="h-3" />
+					</>
+				) : (
+					<>
+						<p className="mt-10 text-center text-white">
+							Enter promt below to get started!
+						</p>
+						<ArrowDownCircleIcon className="h-10 w-10 mx-auto mt-5 animate-bounce text-white" />
+					</>
+				)
 			) : (
-				<>
-					<p className="mt-10 text-center text-white">
-						Enter promt below to get started!
-					</p>
-					<ArrowDownCircleIcon className="h-10 w-10 mx-auto mt-5 animate-bounce text-white" />
-				</>
+				<p className="mt-10 text-center text-white">
+					Some error occured. Please refresh or try again later.
+				</p>
 			)}
 		</div>
 	);
