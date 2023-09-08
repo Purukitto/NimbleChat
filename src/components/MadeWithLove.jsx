93aba1b1a4c2b8ec5430abe6653106ba32b5de73
Purukitto <purukitto@gmail.com>
2023-09-09 01:16:20 +0530

    :sparkles: Made with love

---
+++ src/components/MadeWithLove.jsx
@@ -0,0 +1,13 @@
+export default function MadeWithLove() {
+	return (
+		<div className="text-xs mt-2 text-textSecondary">
+			Made with ♥️ by{" "}
+			<a
+				className=" underline underline-offset-1 font-semibold"
+				href="https://github.com/Purukitto"
+			>
+				Purukitto
+			</a>
+		</div>
+	);
+}
