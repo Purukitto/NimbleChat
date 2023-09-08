fe406468c561b8555303c46c95ba6944c15b6887
Purukitto <purukitto@gmail.com>
2023-09-09 01:21:55 +0530

    Optional center prop

--- src/components/MadeWithLove.jsx
+++ src/components/MadeWithLove.jsx
@@ -1,6 +1,10 @@
-export default function MadeWithLove() {
+export default function MadeWithLove({ center = false }) {
 	return (
-		<div className="text-xs mt-2 text-textSecondary">
+		<div
+			className={`text-xs mt-2 text-textSecondary ${
+				center && "text-center"
+			}`}
+		>
 			Made with ♥️ by{" "}
 			<a
 				className=" underline underline-offset-1 font-semibold"
