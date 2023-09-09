eda3db7130eafef2307336251ac8f93c08a8a956
Purukitto <purukitto@gmail.com>
2023-09-09 18:02:25 +0530

    :bulb: Add comments

--- src/store/geoLocationSlice.js
+++ src/store/geoLocationSlice.js
@@ -1,3 +1,16 @@
+/*
+ * This file contains the redux slice for the geolocation of the user.
+ *
+ * The geolocation slice contains the following state:
+ * 	- latitude: The latitude of the user
+ * 	- longitude: The longitude of the user
+ * 	- error: An error message if any
+ *
+ * The geolocation slice contains the following actions:
+ * 	- setLocation: Sets the latitude and longitude of the user
+ * 	- setError: Sets the error message
+ */
+
 import { createSlice } from "@reduxjs/toolkit";
 
 const geoLocationSlice = createSlice({
